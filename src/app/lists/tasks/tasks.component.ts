c91a57c7d56dbc3d7f5c80e875aab530e2d3b23d
Dmitry Teplov <teplov.work@gmail.com>
2018-03-14 01:56:15 +0300

    feat: Add filtering.

--- src/app/lists/tasks/tasks.component.ts
+++ src/app/lists/tasks/tasks.component.ts
@@ -1,13 +1,25 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { StateService } from 'app/core/state.service';
 import { sanitizeName } from 'app/shared/helpers/sanitize-name';
 import { Task } from 'app/shared/models/task.model';
 import { nameValidators } from 'app/shared/validators/name.validator';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
-import { takeUntil } from 'rxjs/operators';
+import { combineLatest, debounceTime, distinctUntilChanged, map, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs/Subject';
+
+interface Filters {
+  searchQuery: string;
+  // undone = true, all = false
+  undoneOnly: boolean;
+}
+
+const defaultFilters: Filters = {
+  searchQuery: '',
+  undoneOnly: false,
+};
 
 @Component({
   selector: 'atd-tasks',
@@ -18,17 +30,25 @@ import { Subject } from 'rxjs/Subject';
 export class TasksComponent implements OnDestroy {
   public tasks: Observable<Task[]>;
   public form: FormGroup;
+  public searchFilter: FormControl = new FormControl(defaultFilters.searchQuery);
+  public undoneFilter: FormControl = new FormControl(defaultFilters.undoneOnly);
 
+  private filters: BehaviorSubject<Filters> = new BehaviorSubject<Filters>(defaultFilters);
   private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(private route: ActivatedRoute,
               private state: StateService,
               private fb: FormBuilder) {
     this.createForm();
-    this.tasks = state.getActiveListTasks();
+    this.tasks =
+      state.getActiveListTasks().pipe(
+        combineLatest(this.filters),
+        map(([tasks]) => this.filterData(tasks)),
+      );
     this.route.params
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((params) => {
+        this.initFilters();
         this.state.setActiveListId(parseInt(params['id'], 10));
       });
   }
@@ -55,9 +75,61 @@ export class TasksComponent implements OnDestroy {
     }
   }
 
+  private setIsDoneFilter(undoneOnly: boolean): void {
+    this.filters.next({
+      ...this.filters.getValue(),
+      undoneOnly,
+    });
+  }
+
+  private setSearchQuery(searchQuery: string): void {
+    this.filters.next({
+      ...this.filters.getValue(),
+      searchQuery: sanitizeName(searchQuery),
+    });
+  }
+
   private createForm(): void {
     this.form = this.fb.group({
       name: this.fb.control('', nameValidators),
     });
+  }
+
+  private initFilters(): void {
+    // reset filters
+    this.filters.next(defaultFilters);
+    this.searchFilter.reset(defaultFilters.searchQuery);
+    this.undoneFilter.reset(defaultFilters.undoneOnly);
+
+    this.searchFilter.valueChanges.pipe(
+      distinctUntilChanged(),
+      debounceTime(100),
+    ).subscribe((searchQuery) =>
+      this.setSearchQuery(searchQuery),
+    );
+
+    this.undoneFilter.valueChanges.pipe(
+      distinctUntilChanged(),
+    ).subscribe((undoneOnly) =>
+      this.setIsDoneFilter(undoneOnly),
+    );
+  }
+
+  private filterData(tasks: Task[]): Task[] {
+    const searchQuery = this.filters.getValue()
+      .searchQuery.toLocaleLowerCase();
+    // Filter code is duplicated so it can be done in O(n)
+    if (this.filters.getValue().undoneOnly) {
+      return tasks.filter((task) =>
+        task.isDone === false
+        && task.name.toLocaleLowerCase()
+          .search(searchQuery) > -1,
+      );
+    } else {
+      return tasks.filter((task) =>
+        task.name.toLocaleLowerCase()
+          .search(searchQuery) > -1,
+      );
+    }
   }
 }
